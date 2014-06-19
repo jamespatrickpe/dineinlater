6f0bb73493d91b5706896d946725d959f17f9ec9
vicentedemuhammad <vicentedemuhammad@gmail.com>
2014-06-19 12:52:50 +0800

    footer correction

--- application/controllers/footer.php
+++ application/controllers/footer.php
@@ -1,32 +1,37 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-//Loads all Footer items
+// loads all possible footer items
 class Footer extends MY_Controller 
 {
+	// loads the "about" section
 	public function index()
 	{
 		$data['css'] = 'resources/footer.css';
 		$this->loadpage('about',$data);
 	}
 	
+	// loads the "about" section
 	public function about()
 	{
 		$data['css'] = 'resources/footer.css';
 		$this->loadpage('about',$data);
 	}
 	
+	// loads the "contact" section
 	public function contact()
 	{
 		$data['css'] = 'resources/footer.css';
 		$this->loadpage('contact',$data);
 	}
 	
+	// loads the "faq" section
 	public function faq()
 	{
 		$data['css'] = 'resources/footer.css';
 		$this->loadpage('faq',$data);
 	}
 	
+	// loads the "privacy" section
 	public function privacy()
 	{
 		$data['css'] = 'resources/footer.css';
