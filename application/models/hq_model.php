e3bf461215b2e813b3003ba0e1536e11d18ce20c
joojieman <jamespatrickpe@gmail.com>
2014-06-19 23:16:57 +0800

    1116

--- application/models/hq_model.php
+++ application/models/hq_model.php
@@ -5,28 +5,31 @@ class HQ_Model extends MY_Model
 	{	
 	}
 	
-	public function index()
-	{
-		
-	}
-	
 	public function getAll()
 	{
-		
+		$query = $this->db->get('hq');
+        return $this->multipleResults($query);
 	}
 	
-	public function getByID()
+	public function getByID($id)
 	{
-		
+		$this->db->where('hq_id', $id);
+		$query = $this->db->get('hq');
+        return $this->singularResults($query);
 	}
 	
-	public function addHQ()
+	public function addHQ($username, $password, $hqname)
 	{
-		
+			$data = array(
+			   'username' => $username,
+			   'password' => $password,
+			   'hqname' => $hqname
+			);
+		$this->db->insert('customer', $data); 
 	}
 	
-	public function deleteHQ()
+	public function deleteHQ($id)
 	{
-		
+		$this->db->delete('hq', array('hq_id' => $id)); 
 	}
 }
