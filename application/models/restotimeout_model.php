87c2ace03a3848bb5f3867d5ade9136360b88c45
vicentedemuhammad <vicentedemuhammad@gmail.com>
2014-06-21 00:09:02 +0800

    1208

---
+++ application/models/restotimeout_model.php
@@ -0,0 +1,47 @@
+<?php
+class Restotimeout_Model extends MY_Model 
+{
+	public function __construct()
+	{	
+	}
+	
+	// fetches all row data
+	public function getAll()
+	{
+		$query = $this->db->get('restotimeout');
+        return $this->multipleResults($query);
+	}
+	
+	// fetches row data by ID
+	public function getByID($id)
+	{
+		$this->db->where('holiday_id', $id);
+		$query = $this->db->get('restotimeout');
+        return $this->singularResults($query);
+	}
+	
+	// adds row
+	public function addHoliday($restaurant_id, $start_date, $end_date)
+	{
+		$data = array(
+			   'restaurant_id' => $restaurant_id,
+			   'start_date' => $start_date,
+			   'end_date' => $end_date
+			);
+		$this->db->insert('restotimeout', $data); 
+	}
+	
+	// deletes rating by customer id
+	public function deleteHoliday($id)
+	{
+		$this->db->delete('restotimeout', array('holiday_id' => $id)); 
+	}
+	
+	// fetches row by resto ID
+	public function getByResto($id)
+	{
+		$this->db->where('restaurant_id', $id);
+		$query = $this->db->get('restotimeout');
+        return $this->multipleResults($query);
+	}
+}
