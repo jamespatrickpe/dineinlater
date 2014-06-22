fed5835dc477016f85d11b1a33361da753da60d1
vicentedemuhammad <vicentedemuhammad@gmail.com>
2014-06-22 20:17:19 +0800

    0817

--- application/models/reservation_model.php
+++ application/models/reservation_model.php
@@ -22,12 +22,11 @@ class Reservation_Model extends MY_Model
 	}
 	
 	// adds row
-	public function addReservation($resto_ID, $customer_ID, $reservetime , $slots, $note, $date, $time)
+	public function addReservation($resto_ID, $customer_ID , $slots, $note, $date, $time)
 	{
 		$data = array(
 			   'resto_ID' => $resto_ID,
 			   'customer_ID' => $customer_ID,
-			   'reservetime' => $reservetime,
 			   'slots' => $slots,
 			   'confirmed' => '0',
 			   'note' => $note,
@@ -46,17 +45,18 @@ class Reservation_Model extends MY_Model
 	}
 	
 	// updates reservation
-	public function updateReservation($resto_ID, $customer_ID, $reservetime, $slots, $confirmed, $note, $showup, $status)
+	public function updateReservation($resto_ID, $customer_ID, $slots, $confirmed, $note, $showup, $status,  $date, $time)
 	{
 		$data = array(
 			   'resto_ID' => $resto_ID,
 			   'customer_ID' => $customer_ID,
-			   'reservetime' => $reservetime,
 			   'slots' => $slots,
 			   'confirmed' => $confirmed,
 			   'note' => $note,
 			   'showup' => $showup,
-			   'status' => $status
+			   'status' => $status,
+			   'date' => $date,
+			   'time' => $time
 			);
 		$this->db->where('reservation_id', $id);
 		$this->db->update('reservation', $data); 
@@ -83,7 +83,7 @@ class Reservation_Model extends MY_Model
 	// fetches row data by restaurant ID and Status O
 	public function reservationByRestaurantIDOpen($resto_ID)
 	{
-		$sql = "SELECT r.reservation_id, CONCAT(c.firstname,' ',c.lastname) as fullname, r.reservetime, r.slots, r.reservationmade, r.note
+		$sql = "SELECT r.reservation_id, CONCAT(c.firstname,' ',c.lastname) as fullname, r.time, r.date, r.slots, r.reservationmade, r.note
 				FROM reservation r JOIN customer c
 				ON r.customer_id = c.customer_id
 				WHERE r.confirmed = 0
@@ -127,7 +127,7 @@ class Reservation_Model extends MY_Model
 	
 	public function reservationRestoToday($resto_ID)
 	{
-		$sql = "SELECT r.reservation_id, CONCAT(c.firstname,' ',c.lastname) as fullname, r.reservetime, r.slots, r.reservationmade, r.note
+		$sql = "SELECT r.reservation_id, CONCAT(c.firstname,' ',c.lastname) as fullname,  r.time, r.date, r.slots, r.reservationmade, r.note
 				FROM reservation r JOIN customer c
 				ON r.customer_id = c.customer_id
 				WHERE r.confirmed = 1
